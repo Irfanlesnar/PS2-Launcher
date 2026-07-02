29d18570f8cf81838195f0057b9fdba90f44ad95
Irfanlesnar <irfanlesnar@gmail.com>
2026-07-02 05:55:37 +0000

    Add-session-gated-Xbox-input

--- src/menusys.c
+++ src/menusys.c
@@ -1199,6 +1199,7 @@ int gPS5SavedSelectButton = -1;
 int gPS5TempSelectButton = KEY_CIRCLE;
 int gPS5SavedControllerType = -1;
 int gPS5TempControllerType = 0;
+int gPS5XboxInputSessionEnabled = 0;
 int gPS5ControllerLogVisible = 0;
 int gPS5ControllerLogStep = 0;
 int gPS5ControllerLogCaptured = 0;
@@ -2190,10 +2191,12 @@ void menuHandleInputMenu()
             if (getKeyOn(KEY_LEFT)) {
                 sfxPlay(SFX_CURSOR);
                 gPS5TempControllerType = gPS5TempControllerType > 0 ? gPS5TempControllerType - 1 : 3;
+                gPS5XboxInputSessionEnabled = gPS5TempControllerType == 3;
             }
             if (getKeyOn(KEY_RIGHT)) {
                 sfxPlay(SFX_CURSOR);
                 gPS5TempControllerType = gPS5TempControllerType < 3 ? gPS5TempControllerType + 1 : 0;
+                gPS5XboxInputSessionEnabled = gPS5TempControllerType == 3;
             }
             if (getKeyOn(KEY_CROSS) || getKeyOn(gSelectButton)) {
                 int i;
